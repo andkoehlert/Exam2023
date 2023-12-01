b6fd5d60b32e3584fe3b7681ace82207565b2b47
hanacoric <hancor01@easv365.dk>
2023-12-01 05:25:01 +0100

    finished the food-menu page

--- menu.php
+++ menu.php
@@ -18,181 +18,241 @@
     <section class="food">
         <h1>FOOD</h1>
         <div class="food-moto">
-            <p>We make lovely comfort food that is easy to share, using the season’s products.Whoever shares, gets more!
+            <p>We make lovely comfort food that is easy to share, using the season’s products. Whoever shares, gets
+                more!
                 This is the logic behind our kitchen door.</p>
         </div>
     </section>
 
-    <div class="christmas-menu-price">
-        <h1>CHRISTMAS SHARING MENU</h1>
-        <h2> 425 DKK </h2>
-    </div>
-
     <section class="christmas-menu">
+        <div class=" christmas-menu-wrapper">
 
-        <div class="christmas-menu-left-images">
-            <?php $image = get_field("servingimage") ?>
-            <img class="serving-image-left" src="<?php echo $image ?>" alt="">
-            <?php $image = get_field("servingimage") ?>
-            <img class="serving-image-left-bottom" src="<?php echo $image ?>" alt="">
-        </div>
-        <div class="christmas-menu-text">
-            <div>
-                <h3> 1st serving </h3>
-                <?php $text = get_field("1stserving") ?>
-                <p>
-                    <?php echo $text ?>
-                </p>
+            <div class="christmas-menu-left-images">
+
+                <?php $image1 = get_field("1stServingImg") ?>
+                <img class="serving-image-left" src="<?php echo $image1 ?>" alt="">
+
+                <?php $image2 = get_field("2ndServingImg") ?>
+                <img class="serving-image-left-bottom" src="<?php echo $image2 ?>" alt="">
             </div>
 
-            <div>
-                <h3> 2nd serving </h3>
-                <p>Grilled brioche, duck confit & red cabbage Pommes Rôsti with sour cream & chives (Truffles +55) Veal
-                    schnitzel with chantarelles-remoulade & baby sprouts</p>
+            <div class="christmas-menu-text">
+
+                <div class="christmas-menu-price">
+                    <h1>CHRISTMAS SHARING MENU</h1>
+                    <h2> 425 DKK </h2>
+                </div>
+
+                <div class="christmas-menu-price-text">
+                    <div>
+                        <h3 class="christmas-menu-text-left"> 1st serving </h3>
+                        <?php $text1 = get_field("1stserving") ?>
+                        <p>
+                            <?php echo $text1 ?>
+                        </p>
+                    </div>
+
+                    <div>
+                        <h3 class="christmas-menu-text-right"> 2nd serving </h3>
+                        <?php $text2 = get_field("2ndserving") ?>
+                        <p>
+                            <?php echo $text2 ?>
+                        </p>
+                    </div>
+
+                    <div>
+                        <h3 class="christmas-menu-text-left"> 3rd serving </h3>
+                        <?php $text3 = get_field("3rdserving") ?>
+                        <p>
+                            <?php echo $text3 ?>
+                        </p>
+                    </div>
+
+                    <div class="last-christmas-text-wrapper">
+                        <h3 class="christmas-menu-text-right"> 4th serving </h3>
+                        <?php $text4 = get_field("4thserving") ?>
+                        <p>
+                            <?php echo $text4 ?>
+                        </p>
+                    </div>
+
+                </div>
             </div>
 
-            <div>
-                <h3> 3rd serving </h3>
-                <p>Leek, caramelized butter & roasted hazelnuts Hokkaido pumpkin & butternut squash Breast of pork with
-                    lingon berries & puffed pork rind(Opgr. Black Angus with sauce bordelais & pommes frites +125)</p>
+            <div class="christmas-menu-right-images">
 
-            </div>
+                <?php $image3 = get_field("3rdServingImg") ?>
+                <img class="serving-image-right" src="<?php echo $image3 ?>" alt="">
 
-            <div class="last-christmas-text-wrapper">
-                <h3> 4th serving </h3>
-                <p>Rice, vanilla, almonds & cherry</p>
-            </div>
+                <?php $image4 = get_field("4thServingImg") ?>
+                <img class="serving-image-right-bottom" src="<?php echo $image4 ?>" alt="">
 
-        </div>
-
-        <div class="christmas-menu-right-images">
-            <?php $image = get_field("servingimage") ?>
-            <img class="serving-image-right" src="<?php echo $image ?>" alt="">
-            <?php $image = get_field("servingimage") ?>
-            <img class="serving-image-right-bottom" src="<?php echo $image ?>" alt="">
-        </div>
-    </section>
-
-    <div class="christmas-menu-all-in">
-        <h2> ALL IN +195 DKK </h2>
-        <p>Caviar, truffles, sourdough bread & Black Angus (substitutes schnitzel)</p>
-    </div>
-
-    <section class="snacks">
-        <div class="snacks-1st-part">
-            <h1>SNACKS</h1>
-            <div>
-                <p>Pommes frites with herb salt & aioli -</p>
-                <p class="red-text">65</p>
-            </div>
-            <div>
-                <p>2 Oysters au natural or soy & sesame -</p>
-                <p class="red-text">70</p>
-            </div>
-            <div>
-                <p>Pommes frites with parmesan & truffel - </p>
-                <p class="red-text">75</p>
-            </div>
-            <div>
-                <p>Potato croquettes & cheese -</p>
-                <p class="red-text">75</p>
-            </div>
-            <div>
-                <p>Smoked cod with beetroot & blackberry -</p>
-                <p class="red-text">80</p>
             </div>
         </div>
-        <div class="snacks-image">
-            <img class="snacks-image" src="" alt="Snacks">
-        </div>
-        <div class="snacks-2nd-part">
-            <div>
-                <p>Sardines with grilled lemon, aioli & focaccia -</p>
-                <p class="red-text">95</p>
-            </div>
-            <div>
-                <p>Croquettes of pork shank with tarragon mayo -</p>
-                <p class="red-text">95</p>
-            </div>
-            <div>
-                <p>Sliders with crispy chicken & chipotle mayo - </p>
-                <p class="red-text">100</p>
-            </div>
-            <div>
-                <p>PDab with kohlrabi & apple - </p>
-                <p class="red-text">100</p>
-            </div>
-            <div>
-                <p>20 gr. Oscietra Imperial caviar -</p>
-                <p class="red-text">245</p>
-            </div>
+        <div class="christmas-menu-all-in">
+            <h2> ALL IN +195 DKK </h2>
+            <p>Caviar, truffles, sourdough bread & Black Angus (substitutes schnitzel)</p>
         </div>
     </section>
+
     <section class="a-la-carte">
-        <div class="a-la-carte">
+        <div class="a-la-carte-wrapper">
             <h1> A LA CARTE</h1>
         </div>
-        <div class="vegeterian">
-            <h2> VEGETARIAN </h2>
+        <div class="vegeterian-and-meat">
+            <div class="vegeterian">
+                <h2> VEGETARIAN </h2>
+
+                <div>
+                    <p>Hokkaido pumpkin & butternut squash -</p>
+                    <p class="red-text">90</p>
+                </div>
+                <div>
+                    <p>Pommes Rôsti with sour cream & chives - </p>
+                    <p class="red-text">95</p>
+                </div>
+                <div>
+                    <p>Leek, caramelized butter & roasted hazelnuts - </p>
+                    <p class="red-text">105</p>
+                </div>
+            </div>
+
+            <div class="meat">
+                <h2> MEAT </h2>
+                <p>Breast of pork with lingon berries & puffed pork rind - </p>
+                <p class=" red-text">175</p>
+
+                <div>
+                    <p>Veal schnitzel with chantarelles-remoulade & baby sprouts - </p>
+                    <p class="red-text">195</p>
+                </div>
+                <div>
+                    <p>Black Angus flank with sauce bordelais & pommes frites - </p>
+                    <p class="red-text">295</p>
+                </div>
+            </div>
         </div>
-        <div>
-            <p>Hokkaido pumpkin & butternut squash -</p>
-            <p class="red-text">90</p>
+
+        <div class="seafood-cheese-and-dessert">
+            <div class="seafood">
+                <h2> SEAFOOD </h2>
+                <div>
+                    <p>Scallops with cucumber & cream - </p>
+                    <p class="red-text">130</p>
+                </div>
+                <div>
+                    <p>Lobster bisque with green oil & croutons - </p>
+                    <p class="red-text">175</p>
+                </div>
+                <div>
+                    <p>Monkfish with burned tomato, lobster foam & orzotto - </p>
+                    <p class="red-text">295</p>
+                </div>
+            </div>
+            <div class="cheese">
+                <h2> CHEESE & DESSERT </h2>
+                <div>
+                    <p>3 cheeses blackcurrant & crispy bread</p>
+                    <p class="red-text">125</p>
+                </div>
+                <div>
+                    <p>Chocolate mousse with vanilla ice cream & bisquit - </p>
+                    <p class="red-text">95</p>
+                </div>
+                <div>
+                    <p>Rice, vanilla, almonds & cherry - </p>
+                    <p class="red-text">95</p>
+                </div>
+            </div>
         </div>
-        <div>
-            <p>Pommes Rôsti with sour cream & chives - </p>
-            <p class="red-text">95</p>
-        </div>
-        <div>
-            <p>Leek, caramelized butter & roasted hazelnuts - </p>
-            <p class="red-text">105</p>
-        </div>
-        <div class="oysters-image">
-            <img class="oysters-image" src="" alt="Oysters">
-        </div>
-        <div class="seafood">
-            <h2> SEAFOOD </h2>
-        </div>
-        <div>
-            <p>Scallops with cucumber & cream - </p>
-            <p class="red-text">130</p>
-        </div>
-        <div>
-            <p>Lobster bisque with green oil & croutons - </p>
-            <p class="red-text">175</p>
-        </div>
-        <div>
-            <p>Monkfish with burned tomato, lobster foam & orzotto - </p>
-            <p class="red-text">295</p>
-        </div>
-        <div class="meat">
-            <h2> MEAT </h2>
-        </div>
-        <p>Breast of pork with lingon berries & puffed pork rind - </p>
-        <p class="red-text">175</p>
-        </div>
-        <div>
-            <p>Veal schnitzel with chantarelles-remoulade & baby sprouts - </p>
-            <p class="red-text">195</p>
-        </div>
-        <div>
-            <p>Black Angus flank with sauce bordelais & pommes frites - </p>
-            <p class="red-text">295</p>
-        </div>
-        <div class="cheese-&-dessert">
-            <h2> CHEESE & DESSERT </h2>
-        </div>
-        <div>
-            <p>3 cheeses blackcurrant & crispy bread</p>
-            <p class="red-text">125</p>
-        </div>
-        <div>
-            <p>Chocolate mousse with vanilla ice cream & bisquit - </p>
-            <p class="red-text">95</p>
-        </div>
-        <div>
-            <p>Rice, vanilla, almonds & cherry - </p>
-            <p class="red-text">95</p>
+    </section>
+    <section class="snacks">
+        <h1>SNACKS</h1>
+        <div class="snacks-content">
+            <div class="snacks-left-image-wrapper">
+                <div>
+                    <?php $snackImage1 = get_field("snacksImg1") ?>
+                    <img class="snacks-left-image" src="<?php echo $snackImage1 ?>" alt="">
+                </div>
+            </div>
+
+            <div class="snacks-text-wrapper">
+                <div>
+                    <?php $snackText1 = get_field("snackstext1") ?>
+                    <?php $price1 = get_field("price1") ?>
+                    <p>
+                        <?php echo $snackText1 ?>
+                    </p>
+                    <p class="red-text"><?php echo $price1 ?></p>
+                </div>
+                <div>
+                    <?php $snackText2 = get_field("snackstext2") ?>
+                    <?php $price2 = get_field("price2") ?>
+                    <p><?php echo $snackText2 ?></p>
+                    <p class="red-text"><?php echo $price2 ?></p>
+                </div>
+                <div>
+                    <?php $snackText3 = get_field("snackstext3") ?>
+                    <?php $price3 = get_field("price3") ?>
+                    <p><?php echo $snackText3 ?></p>
+                    <p class="red-text"><?php echo $price3 ?></p>
+                </div>
+                <div>
+                    <?php $snackText4 = get_field("snackstext4") ?>
+                    <?php $price4 = get_field("price4") ?>
+                    <p><?php echo $snackText4 ?></p>
+                    <p class="red-text"><?php echo $price4 ?></p>
+                </div>
+                <div>
+                    <?php $snackText5 = get_field("snackstext5") ?>
+                    <?php $price5 = get_field("price5") ?>
+                    <p><?php echo $snackText5 ?></p>
+                    <p class="red-text"><?php echo $price5 ?></p>
+                </div>
+                <div>
+                    <?php $snackText6 = get_field("snackstext6") ?>
+                    <?php $price6 = get_field("price6") ?>
+                    <p><?php echo $snackText6 ?></p>
+                    <p class="red-text"><?php echo $price6 ?></p>
+                </div>
+                <div>
+                    <?php $snackText7 = get_field("snackstext7") ?>
+                    <?php $price7 = get_field("price7") ?>
+                    <p><?php echo $snackText7 ?></p>
+                    <p class="red-text"><?php echo $price7 ?></p>
+                </div>
+                <div>
+                    <?php $snackText8 = get_field("snackstext8") ?>
+                    <?php $price8 = get_field("price8") ?>
+                    <p><?php echo $snackText8 ?></p>
+                    <p class="red-text"><?php echo $price8 ?></p>
+                </div>
+                <div>
+                    <?php $snackText9 = get_field("snackstext9") ?>
+                    <?php $price9 = get_field("price9") ?>
+                    <p><?php echo $snackText9 ?></p>
+                    <p class="red-text"><?php echo $price9 ?></p>
+                </div>
+                <div>
+                    <?php $snackText10 = get_field("snackstext10") ?>
+                    <?php $price10 = get_field("price10") ?>
+                    <p><?php echo $snackText10 ?></p>
+                    <p class="red-text"><?php echo $price10 ?></p>
+                </div>
+                <div>
+                    <?php $snackText11 = get_field("snackstext11") ?>
+                    <?php $price11 = get_field("price11") ?>
+                    <p><?php echo $snackText11 ?></p>
+                    <p class="red-text"><?php echo $price11 ?></p>
+                </div>
+            </div>
+
+            <div class="snacks-right-image-wrapper">
+                <div>
+                    <?php $snackImage2 = get_field("snacksImg2") ?>
+                    <img class="snacks-right-image" src="<?php echo $snackImage2 ?>" alt="">
+                </div>
+            </div>
         </div>
     </section>
 
